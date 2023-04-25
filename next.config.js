cfb5dde79707aa6d8c5c6208b9adba81d75014ea
Poomtum Rattanarat <poomtum2544@hotmail.com>
2023-04-25 07:46:24 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com', 'product-ske.s3.ap-southeast-1.amazonaws.com']
+  },
+  eslint: {
+    ignoreDuringBuilds: true
   }
 }
 
